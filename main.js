b6e45bca9e62714222f8b1f407712e4d350d648c
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-17 22:01:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,14 +25,6 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 
-function speak(){
-    var synth = window.speechSynthesis;
-    speakData1 = "A primeira previsão é " + prediction1;
-    speakData1 = "E a primeira previsão é " + prediction2;
-    var uttherThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
-    synth.speak(utterThis);
-}
-
 function check()
 {
     img = document.getElementById('captured_image')
@@ -44,24 +36,31 @@ function gotResult(error, results) {
         console.error(error);
     } else {
         console.log(results);
-        document.getElementById("resultEmotionName").innerHTML = results[0].label;
+        /*document.getElementById("resultEmotionName").innerHTML = results[0].label;
         document.getElementById("resultEmotionName2").innerHTML = results[1].label;
         prediction1 = results[0].label;
-        prediction2 = results[1].label;
-        speak();
+        prediction2 = results[1].label;*/
+
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        toSpeak = "";
+        
         if(results[0].label == "perfeito")
         {
+            toSpeak = "Isso parece perfeito!";
             document.getElementById("updateEmoji").innerHTML = "&#128076;";
         }
         if(results[0].label == "joinha")
         {
+             toSpeak = "Isso parece joinha!";
             document.getElementById("updateEmoji").innerHTML = "&#128077;";
         }
         if(results[0].label == "vitória")
         {
+             toSpeak = "Isso parece vitória!";
             document.getElementById("updateEmoji").innerHTML = "&#9996;";
         }
-        
+
+        /*repetido
         if(results[1].label == "perfeito")
         {
             document.getElementById("updateEmoji2").innerHTML = "&#128076;";
@@ -74,6 +73,24 @@ function gotResult(error, results) {
         {
             document.getElementById("updateEmoji2").innerHTML = "&#9996;";
         }
+        */
+
+        speak();
     }
 
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    /*speakData1 = "A primeira previsão é " + prediction1;
+    speakData1 = "E a primeira previsão é " + prediction2;
+    var uttherThis = new SpeechSynthesisUtterance(speakData1 + speakData2);*/
+    var synth = window.speechSynthesis;
+    speak_data = toSpeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+    synth.speak(utterThis);
+}
+
+
+
